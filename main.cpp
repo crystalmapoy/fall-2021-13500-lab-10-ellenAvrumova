e2d5a23873dffa45871eba94c8b88aa91318ca18
Ellen Avrumova <ellenavrumova@Ellens-Air.home>
2021-11-19 03:05:50 -0500

    Cleaned up output

--- main.cpp
+++ main.cpp
@@ -18,24 +18,24 @@ int main() {
     now.h = 17;
     now.m = 45;
     Time t = { 17, 45 };
-    std::cout << minutesSinceMidnight(t) << std::endl;
+    std::cout << "At 17:45, it is " << minutesSinceMidnight(t) << " minutes since midnight" << std::endl;
 
     Time morningLecture = {8, 10};
     Time myAlarm;
     myAlarm = morningLecture;
-    std::cout << printTime(morningLecture);
+    std::cout << "The lecture happens at " << printTime(morningLecture);
     std::cout << std::endl;
-    std::cout << printTime(myAlarm);
+    std::cout << "My alarm is set to " << printTime(myAlarm);
     std::cout << std::endl;
 
     Time a = {10, 30};
     Time b = {13, 40};
-    std::cout << minutesUntil(a, b) << std::endl;
+    std::cout << "It is 10:30. There are " << minutesUntil(a, b) << " minutes until 13:40." << std::endl;
 
     std::cout << std::endl;
     std::cout << "***** PART B *****" << std::endl;
     Time c = {8, 10};
-    std::cout << printTime(addMinutes(c, 75));
+    std::cout << "If 75 minutes are added to 8:10, the time would turn into " << printTime(addMinutes(c, 75));
     std::cout << std::endl;
 
     std::cout << std::endl;
@@ -64,18 +64,18 @@ int main() {
     Movie movie6 = {"Black Panther", ACTION, 134};
     TimeSlot oneMovie = {movie5, {14, 10}};
     TimeSlot after = scheduleAfter(oneMovie, movie6);
-    std::cout << after.startTime.h << ":" << after.startTime.m << std::endl;
+    std::cout << "The movie now starts at " << after.startTime.h << ":" << after.startTime.m << std::endl;
     std::cout << getTimeSlot(after) << std::endl;
-    
+
     std::cout << std::endl;
     std::cout << "***** PART E *****" << std::endl;
     Movie movie7 = {"Back to the Future", COMEDY, 90};
     TimeSlot first = {movie7, {10, 00}};
     Movie movie8 = {"Black Panther", ACTION, 90};
     TimeSlot second = {movie8, {11, 30}};
-    std::cout << timeOverlap(first, second) << std::endl;
+    std::cout << timeOverlap(first, second) << " - the movies don't overlap" << std::endl;
     TimeSlot third = {movie8, {11, 29}};
-    std::cout << timeOverlap(first, third) << std::endl;
+    std::cout << timeOverlap(first, third) << " - the movies do overlap" << std::endl;
 
     return 0;
 }
